263fb692ce05b2fc56354cefa22f9e795ffd0f54
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-12-03 22:03:30 +0900

    💄 design: createGlobalStyle을 이용하여 전체 스타일 초기화 및 지정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
